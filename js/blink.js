2a7bd5492a02389c55f7893274d0dc099ff71ed8
ErikvanderVeen1994 <erikpetervanderveen@hotmail.com>
2016-01-20 21:51:53 +0100

    nieuw
    nieuw

---
+++ js/blink.js
@@ -0,0 +1,12 @@
+/*(function() {
+			    var s = document.getElementById('blinkcover').style,
+			        f = false,
+			        c1 = '#FFFFFF',
+			        c2 = '#F2F2F2';
+
+			    setInterval(function() {
+			        s.backgroundColor = f ? c1 : c2;
+			        s.color = f ? c2 : c1;
+			        f = !f;
+			    }, 200);
+				})();*/
